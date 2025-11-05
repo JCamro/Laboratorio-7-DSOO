fa1fe189ea09126e3dfe204783d6e8e93d6c417f
Marco Salas <marcolex30@gmail.com>
2025-11-05 01:29:04 -0500

    segunda version de class Cuenta

--- Cuenta.java
+++ Cuenta.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Cuenta {
     private int numeroCuenta;
@@ -6,6 +7,7 @@ public class Cuenta {
     private double saldo;
     private int clave;
     private LocalDateTime fechaApertura;
+    private ArrayList<Transaccion> listaTransacciones = new ArrayList<>();
     
     public Cuenta(int numeroCuenta, String tipoCuenta, double saldo, int clave) {
         setNumeroCuenta(numeroCuenta);
@@ -23,14 +25,18 @@ public class Cuenta {
     //GETTERS
     public LocalDateTime getFechaApertura() {return fechaApertura;}
     public int getNumeroCuenta() {return numeroCuenta;}
-    public double consultarSaldo() {return saldo;}
+    public double getSaldo() {return saldo;}
     public String getTipoCuenta() {return tipoCuenta;}
     public int getClave() {return clave;}
 
     //DEPOSITA DINERO
-    public void deposito(double monto) {this.saldo-=monto;}
+    public void deposito(double monto) {this.saldo+=monto;}
     //RETIRA DINERO
     public void retiro(double monto) {this.saldo-=monto;}
+    // Añade transacciones
+    public void añadirTransaccion(Transaccion transaccion) {
+        this.listaTransacciones.add(transaccion);
+    }
 
     public String toString() {
         return "---Contrado de apertura de Cuenta---"+
